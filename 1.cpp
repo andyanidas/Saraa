6f906fbe08156ca813c7398f9dd715d9b44b2966
Lkhagva-erdene <anidas.com@gmail.com>
2021-02-05 23:09:15 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x = 500,y = 9898,z;				//declaring z variable integer
+	z = x * y;
+	cout<<"z = "<<z<<endl<<"x = "<<x<<"hello bla bla asdaegerbh";	//Printing
+
+	
+	
+	
+	return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//+ - * /
+	int a,b;
+	a = 100;
+	b = a*2;
+	cout<<"a = "<<a+100<<endl<<"b = "<<b<<endl;
+	a = a + 100 + a + b;
+	cout<<a<<endl;
+	b = a*2;
+	cout<<b;
+	
+	return 0;
+}
