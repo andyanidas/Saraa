7a2a25461aa36f1501ae1ba8ca7bf4f2bbcabe55
Lkhagva-erdene <anidas.com@gmail.com>
2021-02-10 22:58:26 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//Numbers
+	int x = 5.56546;		//declaring integer variable
+	double d = 5.565465465495;
+	float f = 5.316511;
+	//boolean
+	bool check = false;		//boolean can store only two values 0 or 1 ->> false or true
+	//characters
+	char c = '2';
+	//string
+	string sentence = "c = ";
+	cout<<sentence<<c<<endl;
+	//
+	
+	
+	cout<<"Check = "<<check<<endl;
+	cout<<"x = "<<x<<"\nd = "<<d<<endl<<"f = "<<f;
+	
+	
+	
+	return 0;
+}
---
+++ 4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x,y,sum;
+	cout<<"Enter first integer: ";
+	cin>>x;								//a
+	cout<<"Enter Second integer: ";
+	cin>>y;
+	
+	cout<<"Sum of two number is :"<<x<<y;
+	
+	//%-->> finding remainder	ex: 15%9 = 6
+	// 1651 % 100 = 51 // 6519%2 = 1 ODD // 12314 %2 = 0 --EVEN
+	
+	
+	return 0;
+}
